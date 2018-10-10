d83fae940b33e39a216fafa4c7a6e69a194d68b8
Sebastian <sebgertz@gmail.com>
2018-10-10 18:06:49 -0400

    added print_blocks and add_block methods

--- blockchain.py
+++ blockchain.py
@@ -11,4 +11,16 @@ class Blockchain:
       transactions = []
       previous_hash = "0"
       self.chain.append(Block(transactions, previous_hash))
-      
+
+    # prints contents of blockchain
+    def print_blocks(self):
+      for i in range(len(self.chain)):
+        current_block = self.chain[i]
+        print("Block {} {}".format(i, current_block))
+        current_block.print_contents()
+
+    # add block to blockchain `chain`
+    def add_block(self, transactions):
+      new_block = Block(transactions, self.chain[len(self.chain) - 1].hash)
+      self.chain.append(new_block)
+      pass
