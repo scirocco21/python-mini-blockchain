56baf6a15d7964e28a70e4091b9cef0595282b9c
Sebastian <sebgertz@gmail.com>
2018-10-10 21:59:17 -0400

    added basic proof strategy to show difficulty requirement has been met

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from blockchain import Blockchain
+from hashlib import sha256
 
 # instantiate new blockchain with genesis block
 my_blockchain = Blockchain()
@@ -13,5 +14,22 @@ my_blockchain.print_blocks()
 # try to change the first block's transaction record with some other value
 my_blockchain.chain[1].transactions = "fake_transactions"
 
-# basic validate_chain method will return false: the current hash of the block will not equal the generated hash of the block.
+# basic validate_chain method will return false: the current hash of the block will not equal the generated hash of the blockn (in other words, its initial correct value does not match the hash that would now be produced with the false record)
 my_blockchain.validate_chain()
+
+# a nonce ("number only used once") is a number added to a hashed block that, when rehashed, meets the difficulty level restrictions
+# In this simplified example, the nonce is increased to meet a difficulty of two, where 2 refers to two leading zeros in the hash
+new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},
+               	{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
+
+# sets the amount of leading zeros that must be found in the hash produced by the nonce
+difficulty = 2
+nonce = 0
+# creating the proof
+proof = sha256(str(nonce).encode() + str(new_transactions).encode()).hexdigest()
+print(proof)
+# finding a proof that has 2 leading zeros
+while proof[:2] != '0'*difficulty:
+  nonce += 1
+  proof = sha256(str(nonce).encode() + str(new_transactions).encode()).hexdigest()
+# the nonce value can be in the hundreds before the while loop terminates and the difficulty requirement is met
